969903861a74418d73ec6164641de5e8fb548cd5
Kent Zhang <kentzhang31@yahoo.com>
2021-10-29 13:50:48 -0700

    seasons played header

--- src/scripts/dynamic_chart.js
+++ src/scripts/dynamic_chart.js
@@ -5,10 +5,18 @@ async function accessData(playername) {
   console.log(data);
 }
 
+function displayYearsHeader() {
+  let container = document.getElementById("years-header");
+  let child = document.createElement('h1');
+  child.innerHTML = "Seasons Played";
+  container.appendChild(child);
+}
+
 function displayYearsClickable(rookieYear) {
   let currentYear = new Date().getFullYear();
   let btnsContainer = document.getElementById("years-btns");
 
+  displayYearsHeader();
   for(let i = rookieYear; i < currentYear + 1; i++) {
     let childButton = document.createElement('div');
     childButton.className = "year-btn";
